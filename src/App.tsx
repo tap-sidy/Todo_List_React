033e90d0f78705ef4f626f3977490c1948a16b02
tap-sidy <tapilysidy@gmail.com>
2026-06-23 20:19:26 +0100

    ajout de fonction (save,ecriture dans le input ...)

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+
+import { useEffect, useState } from "react";
+
 type Priority = "Urgente" | "Moyenne" | "Basse";
 
 type Todo = {
@@ -7,6 +10,39 @@ type Todo = {
 };
 
 function App() {
+  const [input, setINPUT] = useState("");
+  const [priority, setPRIORITY] = useState <Priority>("Moyenne");
+
+  const savedTodos = localStorage. getItem("todos")
+  const initialTodos = savedTodos ? JSON. parse (savedTodos) :[]
+
+  const [todos, setTODOS] = useState <Todo[]>([initialTodos])
+  
+  useEffect(() => {
+    localStorage.setItem("todos",JSON.stringify(todos))
+  }, [todos])
+  
+
+  function addTodo() {
+    if (input.trim() == "") {
+      return;
+    }
+    const newTodo :Todo = {
+      id: Date.now(),
+      text: input.trim(),
+      priority: priority,
+    }
+
+    const newTodos = [newTodo, ...todos]
+    setTODOS(newTodos)
+    setINPUT("")
+    setPRIORITY("Moyenne")
+    console.log(newTodos);
+    
+    
+  }
+
+  let filteredtodos : Todo []= []
   return (
     <div className="flex justify-center">
       <div className="w-2/3 flex flex-col gap-4 my-15 bg-base-300 p-5 rounded-2x1">
@@ -15,15 +51,28 @@ function App() {
             type="text"
             className="input w-full"
             placeholder="Add Yours task"
+            value={input}
+            onChange={(e) => setINPUT(e.target.value)}
           />
 
-          <select className="select w-full">
+          <select
+            className="select w-full"
+            value={priority}
+            onChange={(e) => setPRIORITY(e.target.value as Priority)}
+          >
             <option value="Urgente">Urgente</option>
             <option value="Moyenne">Moyenne</option>
             <option value="Basse">Basse</option>
           </select>
 
-          <button className="btn btn-primary">Add</button>
+          <button className="btn btn-primary" onClick={addTodo}>
+            Add
+          </button>
+        </div>
+        <div className="space-y-2 flex-1 h-fit">
+          <div>
+             
+          </div>
         </div>
       </div>
     </div>
